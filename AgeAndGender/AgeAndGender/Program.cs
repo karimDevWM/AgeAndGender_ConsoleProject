1d7ab3c31e79758fd69c7106d527636af9e8fbb5
karimdevwm <ka.trabelsi9@gmail.com>
2021-05-11 13:26:56 +0200

    first commit of AgeAndGender_ConsoleProject

---
+++ AgeAndGender/AgeAndGender/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace AgeAndGender
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("saisir h pour homme ou f pour femme : ");
+            string genre = Console.ReadLine();
+            Console.WriteLine("saisir age : ");
+            int age = Convert.ToInt32(Console.ReadLine());
+
+            if(genre.Equals("h") && age>=10)
+            {
+                Console.WriteLine("homme");
+            }
+            else if(genre.Equals("f") && age>=18)
+            {
+                Console.WriteLine("femme");
+            }
+            else if(genre.Equals("h") && age<18)
+            {
+                Console.WriteLine("garçon");
+            }
+            else if(genre.Equals("f") && age<18)
+            {
+                Console.WriteLine("fille");
+            }
+            else
+            {
+                Console.WriteLine("pas compris");
+            }
+        }
+    }
+}
